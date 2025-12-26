cc239590ed39b76b336c6bdf91ce952bf771cef5
Anirban Das <72315775+anirban12d@users.noreply.github.com>
2025-12-26 17:54:37 +0530

    fix(frontend): updated the notfound component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Toaster } from '@/components/ui/sonner'
 import { AuthProvider } from '@/features/auth'
+import { NotFound } from '@/components/not-found'
 
 export const Route = createRootRoute({
   component: RootComponent,
+  notFoundComponent: NotFound,
 })
 
 function RootComponent() {
